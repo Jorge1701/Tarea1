e84eecc150923e851a4181181c743f42531bb75d
Axz501 <axgrarzu@gmail.com>
2017-03-21 18:37:53 -0300

    Add files via upload

---
+++ Empresa.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Empresa.h
+ * Author: Axel
+ *
+ * Created on 21 de marzo de 2017, 17:30
+ */
+
+#ifndef EMPRESA_H
+#define EMPRESA_H
+#include <iostream>
+#include <string>
+
+class Empresa {
+private:
+    string id;
+    
+};
+
+
+
+#endif /* EMPRESA_H */
+
