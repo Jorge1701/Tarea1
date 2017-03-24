999ea4b17c9741fddc587918710796e618afff71
alepeculio <alejandropeculio@gmail.com>
2017-03-24 16:41:25 -0300

    Update RelacionLaboral.cpp

--- Tarea1/Sources/RelacionLaboral.cpp
+++ Tarea1/Sources/RelacionLaboral.cpp
@@ -1,33 +1,47 @@
 #include "../Headers/RelacionLaboral.h"
-
+#include "../Headers/Nacional.h"
+#include "../Headers/Extranjera.h"
+#include "../Headers/Empresa.h"
 
 RelacionLaboral::RelacionLaboral(float sueldo,Fecha* fecha,Empresa* Em){
 	this->sueldo=sueldo;
 	this->fechaDesvinculacion=fecha;
 	this->Em=Em;
 }
+RelacionLaboral::RelacionLaboral(){
+    this->sueldo=0;
+    this->fechaDesvinculacion=NULL;
+    this->Em=NULL;
+} 
 
-float RelacionLaboral::getSueldoLiquido(){
+float RelacionLaboral::getSueldo(){
 	return sueldo;
 }
-Fecha RelacionLaboral::*getFechaDesvinculacion(){
-	return *Fecha;
+Fecha* RelacionLaboral::getFechaDesvinculacion(){
+	return  fechaDesvinculacion;
 }
 Empresa* RelacionLaboral::getEm(){
-	return *Em;
+	return Em;
 }
 
+float RelacionLaboral::getSueldoLiquido(){
+    float sueldoLiquido;
+    sueldoLiquido=this->sueldo * Em->getDescuento();
+    return sueldoLiquido;
+}
 
 void RelacionLaboral::setSueldo(float sueldo){
 	this->sueldo=sueldo;
 }
-void RelacionLaboral::setFecha(Fecha *fechaDesvinculacion){
-	this->*fechaDesvinculacion= *fechaDesvinculacion;
+void RelacionLaboral::setFecha(Fecha* fechaDesvinculacion){
+	this->fechaDesvinculacion= fechaDesvinculacion;
 }
 void RelacionLaboral::setEm(Empresa* Em){
 	this->Em=Em;
 }
-RelacionLaboral ::~RelacionLaboral(){}
+RelacionLaboral ::~RelacionLaboral(){
+delete fechaDesvinculacion;
+}
 
 
 
